db8f286084c1e354101f0ad7e7250ef506c46e8b
Valentin Nicolae <nicolae9931@yahoo.com>
2019-09-10 01:57:32 +0100

    First push

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const addNumberContainer = document.querySelector('budget')
+
+var Holiday1 = {
+  name: 'Dubai'
+  budget: 3000
+}
+var Holiday2 = {
+  name: 'Dubai'
+  budget: 6000
+}
+
+function calculateCost (holiday){
+  
+}
+
+
+
+
+
+// Array 
+//   const holiday = document.createElement('div')
+//   const name = 'Dubai'
+//   const budget = '$'2500
+//   holiday.innerText =  name, budget 
+
