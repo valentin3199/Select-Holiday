0036900993783e84713c28934c24cef47cbfc7fc
Valentin Nicolae <nicolae9931@yahoo.com>
2019-10-24 02:10:45 +0100

    Learn JavaScript

--- src/script.js
+++ src/script.js
@@ -1,6 +1,123 @@
-function see() {
-    var fr = document.querySelector('.first');
-    var sc =document.querySelector('.second');
-    var frsc = fr + sc;
-    document.querySelector('#result').innerHtml = frsc;
+
+
+
+/*
+||||||||||||||||||||
+const first =  String(1);
+const second = 2;
+const all = first + second;
+
+console.log(all);
+console.log(typeof all);
+||||||||||||||||||||||||||||||||
+let val;
+//Number to string
+val = String(5);//Convert number in String
+val = String(4+4)
+
+//Bool in string
+val = String(true);
+
+//Date to String
+val = String(new Date());
+
+//Array to string
+val = String([1,2,3,4]);
+//to String
+val = (5).toString()
+
+///String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('Hello');
+val = Number([1,2,4]);
+
+val = parseInt('100');
+val = parseFloat('100.31');
+//Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed());
+
+///////Reference types - Objects\\\\\\\\
+//Array
+const hobbies = ['movies','music'];
+||||||||||||||||||||||
+
+//Object literal
+const address = {
+    city: 'Chingford',
+    state: 'An',
 }
+const today = new Date(); //All details for Today
+console.log(today)
+console.log(typeof address);
+||||||||||||||||||||||
+
+//////// Primitive\\\\\\\\
+//String
+const name = 'Robert';
+console.log(typeof name); 
+
+//Number
+const age = 44;
+console.log(typeof age);
+
+//Boolean
+const bol = true;
+console.log(typeof bol);
+
+//Null
+const nothing = null;
+console.log(typeof nothing);
+
+//Undefined
+let test;
+console.log(typeof test);
+
+//Symbol
+const sym = Symbol();
+console.log(typeof sym);
+//||||||||||||||||||||||
+||||||||||||||||||||||
+const number = [1,2,3,4];
+number.push(7);
+console.log(number);
+
+|||||||||Object|||||||||||||
+const person = {
+    name: 'Sara',
+    age: 30
+}
+person.name = 'Radarada';
+person.age = 23;
+console.log(person);
+
+||||||||||||||||||||||
+const name = 'Jhon';
+console.log(name);
+const = 'Rada'; /// This is ERROR
+
+||||||||||||||||||||||
+var firstName = 'mouse';
+console.log(firstName);
+
+||||||||||||||||||||||
+var single;
+single = 'one';
+console.log(single);
+||||||||||||||||||||||
+
+var name = 'Valentin';
+console.log(name);
+name = 'Nicolae';
+console.log(name);
+||||||||||||||||||||||
+
+console.table({a:1, a:2});    //table
+document.querySelector('h1').style.color = 'red';
+document.querySelector('h1').style.fontSize = '23px';
+*/
